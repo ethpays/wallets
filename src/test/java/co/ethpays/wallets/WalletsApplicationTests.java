afd6ffbe5dfc33c2b49d9f607235cc3eb5a00159
fanto6314 <antonio.foresta04@gmail.com>
2024-07-23 13:35:26 +0200

    first commit

---
+++ src/test/java/co/ethpays/wallets/WalletsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ethpays.wallets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
