bda7b23e4cc86ee56bec5fd34638677be9b9a22c
fanto6314 <antonio.foresta04@gmail.com>
2024-07-23 13:50:23 +0200

    initial setup

--- src/main/java/co/ethpays/wallets/WalletsApplication.java
+++ src/main/java/co/ethpays/wallets/WalletsApplication.java
@@ -1,9 +1,14 @@
+/* 2024 © Copyright, Ethpays (Antonio Foresta), All rights reserved. */
 package co.ethpays.wallets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableScheduling
 public class WalletsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/co/ethpays/wallets/api/WebConfig.java
@@ -0,0 +1,18 @@
+package co.ethpays.wallets.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //TODO:add interceptors
+    }
+}
